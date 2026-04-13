a1a4ade037747b73739b4348288edda47800ae5c
Julian Carvajal <julianc.carvajal12@gmail.coml>
2026-04-13 16:16:33 -0500

    feat(auth): missed files added

---
+++ src/services/api.ts
@@ -0,0 +1,34 @@
+import { clearAuthSession, getStoredAuthToken } from '@/auth/auth.session'
+
+export interface ApiFetchOptions extends RequestInit {
+  auth?: boolean
+}
+
+export async function apiFetch(input: RequestInfo | URL, init: ApiFetchOptions = {}): Promise<Response> {
+  const { auth = true, headers, ...requestInit } = init
+  const requestHeaders = new Headers(headers)
+
+  if (auth) {
+    const token = getStoredAuthToken()
+
+    if (token && !requestHeaders.has('Authorization')) {
+      requestHeaders.set('Authorization', `Bearer ${token}`)
+    }
+  }
+
+  if (!requestHeaders.has('Accept')) {
+    requestHeaders.set('Accept', 'application/json')
+  }
+
+  const response = await fetch(input, {
+    ...requestInit,
+    headers: requestHeaders,
+    credentials: requestInit.credentials ?? 'include',
+  })
+
+  if (response.status === 401) {
+    clearAuthSession()
+  }
+
+  return response
+}
